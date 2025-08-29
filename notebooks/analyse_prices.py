72a13934b18eef85b75f28f963b9342c85516306
dttruong@dius.com.au <dttruong@dius.com.au>
2025-08-29 06:33:25 +0000

    Compute the number of zero claims

--- notebooks/analyse_prices.py
+++ notebooks/analyse_prices.py
@@ -2,19 +2,54 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+from collections import defaultdict
+from tqdm import tqdm
 
 # COMMAND ----------
 
-df = spark.sql("SELECT * from lakehouse_development.ml_features.analysis_job_lead_prices").toPandas()
-len(df)
+num_leads = spark.sql("SELECT count(*) from lakehouse_development.ml_features.analysis_job_lead_prices").toPandas()
+num_leads = num_leads.iloc[0][0]
 
 # COMMAND ----------
 
-len(df), len(df["job_id"].unique()), len(df["lead_id"].unique())
+# Count the number jobs
+num_jobs = spark.sql("SELECT count(DISTINCT job_id) from lakehouse_development.ml_features.analysis_job_lead_prices").toPandas()
+num_jobs = num_jobs.iloc[0][0]
 
 # COMMAND ----------
 
-num_claims = df.groupby("job_id")["job_lead_claimed"].sum()
+print(num_leads, num_jobs, num_leads / num_jobs)
+
+# COMMAND ----------
+
+# Fetch the claimed leads
+claimed_leads = spark.sql("SELECT * from lakehouse_development.ml_features.analysis_job_lead_prices where job_lead_claimed = true").toPandas()
+len(claimed_leads)
+
+# COMMAND ----------
+
+num_claimed_leads = len(claimed_leads)
+num_clained_jobs = len(claimed_leads["job_id"].unique())
+print(num_claimed_leads, num_clained_jobs, num_claimed_leads / num_clained_jobs)
+
+# COMMAND ----------
+
+zero_claims = num_jobs - num_clained_jobs
+print(zero_claims, zero_claims / num_jobs)
+
+# COMMAND ----------
+
+num_jobs = len(job_prices["job_id"].unique())
+job_prices["job_id"].nunique()
+# Count number of
+
+# COMMAND ----------
+
+len(job_prices), len(job_prices["job_id"].unique()), len(job_prices["lead_id"].unique())
+
+# COMMAND ----------
+
+num_claims = job_prices.groupby("job_id")["job_lead_claimed"].sum()
 num_claims.hist(range=(0, 10))
 num_claims.describe()
 
@@ -24,9 +59,39 @@ np.percentile(num_claims, 99), np.percentile(num_claims, 95)
 
 # COMMAND ----------
 
+# Count number of jobs and number of leads per job
+num_jobs = len(job_prices["job_id"].unique())
+num_leads = len(job_prices["lead_id"].unique())
+print(num_jobs, num_leads, num_leads / num_jobs)
+
+# COMMAND ----------
+
+# Count number of cases
+# case 1: all claims in first round first batch
+# case 2: all claims in first round first batch and first round other batches
+# case 3: no claims in first round
+cases = defaultdict(int)
+for job_id, group in tqdm(job_prices.groupby(by="job_id")):
+    claimed_in = (group["claimed_in"]).unique()
+    if len(claimed_in) == 1 and "first-round-first-batch" in claimed_in:
+        cases["case_1"] += 1
+    elif len(claimed_in) == 2 and "first-round-first-batch" in claimed_in and "first-round-other-batches" in claimed_in:
+        cases["case_2"] += 1
+    else:
+        cases["others"] += 1
+print(cases)
+
+
+# COMMAND ----------
+
+for case in cases:
+    print(case, cases[case], cases[case] / sum(cases.values()))
+
+# COMMAND ----------
+
 # Compute discounts for each job having the first-round-first-batch and first-round-other-batches
 discount_rows = []
-for idx, group in df.groupby(by="job_id"):
+for idx, group in job_prices.groupby(by="job_id"):
     if len(group) > 1 and "first-round-first-batch" in list(group["claimed_in"]) and "first-round-other-batches" in list(group["claimed_in"]):
         first_batch = group[group["claimed_in"] == "first-round-first-batch"]
         other_batches = group[group["claimed_in"] == "first-round-other-batches"]
@@ -36,6 +101,10 @@ for idx, group in df.groupby(by="job_id"):
         discount_percentage = absolute_discount / first_batch_price * 100
         discount_rows.append(dict(first_batch_price=first_batch_price, mean_price=mean_price, absolute_discount=absolute_discount, discount_percentage=discount_percentage))
 discount_df = pd.DataFrame(discount_rows)
+
+# COMMAND ----------
+
+len(discount_df), len(discount_df) / len(job_prices["job_id"].unique())
 
 # COMMAND ----------
 
@@ -52,6 +121,14 @@ ax.hist(discount_df["discount_percentage"], range=(-60, 60), bins=range(-60, 60,
 ax.set_xlabel("Discount percentage")
 ax.set_ylabel("Number of jobs")
 ax.grid("both")
+
+# COMMAND ----------
+
+job_types = spark.sql("SELECT * FROM lakehouse_production.ml_features.over_under_claim_raticate_payload where date > '2025-01-00'").toPandas()
+
+# COMMAND ----------
+
+len(job_types)
 
 # COMMAND ----------
 
