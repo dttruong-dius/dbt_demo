15f140f0c3b1bbc4ae2936fdd15e52d74b26b612
dttruong@dius.com.au <dttruong@dius.com.au>
2025-08-29 07:02:05 +0000

    Update date and variable names

--- notebooks/analyse_prices.py
+++ notebooks/analyse_prices.py
@@ -39,17 +39,7 @@ print(zero_claims, zero_claims / num_jobs)
 
 # COMMAND ----------
 
-num_jobs = len(job_prices["job_id"].unique())
-job_prices["job_id"].nunique()
-# Count number of
-
-# COMMAND ----------
-
-len(job_prices), len(job_prices["job_id"].unique()), len(job_prices["lead_id"].unique())
-
-# COMMAND ----------
-
-num_claims = job_prices.groupby("job_id")["job_lead_claimed"].sum()
+num_claims = claimed_leads.groupby("job_id")["job_lead_claimed"].sum()
 num_claims.hist(range=(0, 10))
 num_claims.describe()
 
@@ -59,19 +49,12 @@ np.percentile(num_claims, 99), np.percentile(num_claims, 95)
 
 # COMMAND ----------
 
-# Count number of jobs and number of leads per job
-num_jobs = len(job_prices["job_id"].unique())
-num_leads = len(job_prices["lead_id"].unique())
-print(num_jobs, num_leads, num_leads / num_jobs)
-
-# COMMAND ----------
-
 # Count number of cases
 # case 1: all claims in first round first batch
 # case 2: all claims in first round first batch and first round other batches
 # case 3: no claims in first round
 cases = defaultdict(int)
-for job_id, group in tqdm(job_prices.groupby(by="job_id")):
+for job_id, group in tqdm(claimed_leads.groupby(by="job_id")):
     claimed_in = (group["claimed_in"]).unique()
     if len(claimed_in) == 1 and "first-round-first-batch" in claimed_in:
         cases["case_1"] += 1
@@ -91,7 +74,7 @@ for case in cases:
 
 # Compute discounts for each job having the first-round-first-batch and first-round-other-batches
 discount_rows = []
-for idx, group in job_prices.groupby(by="job_id"):
+for idx, group in claimed_leads.groupby(by="job_id"):
     if len(group) > 1 and "first-round-first-batch" in list(group["claimed_in"]) and "first-round-other-batches" in list(group["claimed_in"]):
         first_batch = group[group["claimed_in"] == "first-round-first-batch"]
         other_batches = group[group["claimed_in"] == "first-round-other-batches"]
@@ -104,7 +87,7 @@ discount_df = pd.DataFrame(discount_rows)
 
 # COMMAND ----------
 
-len(discount_df), len(discount_df) / len(job_prices["job_id"].unique())
+len(discount_df), len(discount_df) / len(claimed_leads["job_id"].unique())
 
 # COMMAND ----------
 
@@ -124,7 +107,7 @@ ax.grid("both")
 
 # COMMAND ----------
 
-job_types = spark.sql("SELECT * FROM lakehouse_production.ml_features.over_under_claim_raticate_payload where date > '2025-01-00'").toPandas()
+job_types = spark.sql("SELECT * FROM lakehouse_production.ml_features.over_under_claim_raticate_payload where date >= '2025-01-01'").toPandas()
 
 # COMMAND ----------
 
