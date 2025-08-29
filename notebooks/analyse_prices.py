a9c99a8be23e8d570618aa2f046d98f77e9cb664
dttruong@dius.com.au <dttruong@dius.com.au>
2025-08-29 04:03:56 +0000

    Compute the absolute discount and discount percentage

--- notebooks/analyse_prices.py
+++ notebooks/analyse_prices.py
@@ -1,5 +1,7 @@
 # Databricks notebook source
 from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
 
 # COMMAND ----------
 
@@ -12,14 +14,44 @@ len(df), len(df["job_id"].unique()), len(df["lead_id"].unique())
 
 # COMMAND ----------
 
-count = 0
+num_claims = df.groupby("job_id")["job_lead_claimed"].sum()
+num_claims.hist(range=(0, 10))
+num_claims.describe()
+
+# COMMAND ----------
+
+np.percentile(num_claims, 99), np.percentile(num_claims, 95)
+
+# COMMAND ----------
+
+# Compute discounts for each job having the first-round-first-batch and first-round-other-batches
+discount_rows = []
 for idx, group in df.groupby(by="job_id"):
-    if len(group) > 1 and group["is_other_rounds"].sum() > 0:
-        print(idx)
-        display(group)
-        count += 1
-    if count > 3:
-        break
+    if len(group) > 1 and "first-round-first-batch" in list(group["claimed_in"]) and "first-round-other-batches" in list(group["claimed_in"]):
+        first_batch = group[group["claimed_in"] == "first-round-first-batch"]
+        other_batches = group[group["claimed_in"] == "first-round-other-batches"]
+        mean_price = group["job_lead_price"].mean()
+        first_batch_price = first_batch["job_lead_price"].mean()
+        absolute_discount = first_batch_price - mean_price
+        discount_percentage = absolute_discount / first_batch_price * 100
+        discount_rows.append(dict(first_batch_price=first_batch_price, mean_price=mean_price, absolute_discount=absolute_discount, discount_percentage=discount_percentage))
+discount_df = pd.DataFrame(discount_rows)
+
+# COMMAND ----------
+
+fig, ax = plt.subplots()
+ax.hist(discount_df["absolute_discount"], bins=range(-40, 60, 10))
+ax.set_xlabel("Absolute discount")
+ax.set_ylabel("Number of jobs")
+ax.grid("both")
+
+# COMMAND ----------
+
+fig, ax = plt.subplots()
+ax.hist(discount_df["discount_percentage"], range=(-60, 60), bins=range(-60, 60, 10))
+ax.set_xlabel("Discount percentage")
+ax.set_ylabel("Number of jobs")
+ax.grid("both")
 
 # COMMAND ----------
 
